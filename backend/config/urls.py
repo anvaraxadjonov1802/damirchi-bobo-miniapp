cdc6d0da552f0a2d0ca983e6283b97376691cfe3
Anvar Axadjonov <axadjonov123@gmail.com>
2026-05-25 13:11:48 +0500

    Serve media files for demo

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -12,5 +12,11 @@ urlpatterns = [
     path("api/", include("orders.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Demo uchun media files serve qilish
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
