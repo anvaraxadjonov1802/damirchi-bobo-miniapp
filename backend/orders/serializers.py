f39a3fc16f9ec64cce4f5aaf632ca56f49dc3f44
Anvar <101347819+anvaraxadjonov1802@users.noreply.github.com>
2026-08-09 00:44:17 +0500

    Use MongoDB transaction API for order creation

--- backend/orders/serializers.py
+++ backend/orders/serializers.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.core.exceptions import ValidationError as DjangoValidationError
-from django.db import transaction
+from django_mongodb_backend import transaction
 from rest_framework import serializers
 
 from common.models import RestaurantSettings
