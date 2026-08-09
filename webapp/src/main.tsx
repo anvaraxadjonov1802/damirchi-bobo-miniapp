a8b5896f4919348df27ae1093d0c79de3cbfb0dd
Anvar <101347819+anvaraxadjonov1802@users.noreply.github.com>
2026-08-10 02:12:46 +0500

    Enable fast cached menu startup before React mounts

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
+import { client } from "./api/client";
+import { enableFastStartup } from "./api/fastStartup";
 import { ToastProvider } from "./components/ToastProvider";
 
 import "./index.css";
@@ -11,6 +13,8 @@ const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Root element not found");
 }
+
+enableFastStartup(client);
 
 if ("serviceWorker" in navigator && import.meta.env.PROD) {
   navigator.serviceWorker.register("/sw.js").catch((error) => {
